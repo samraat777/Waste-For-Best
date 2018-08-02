1cdc6b1a1fd8580d78cab894c408e605605b51df
samraat777 <samraatanand77@gmail.com>
2018-08-02 18:11:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sam/wasteforbest/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.sam.wasteforbest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    public void googleOnclick(View view)
+    {
+    Intent intent=new Intent(this,googleSignUp.class);
+    startActivity(intent);
+    }
+    public void phoneOnclick(View view)
+    {
+        Intent intent=new Intent(this,phoneSignUp.class);
+        startActivity(intent);
+    }
+    public void gmailOnclick(View view)
+    {
+        Intent intent=new Intent(this,gmailSignUp.class);
+        startActivity(intent);
+    }
+    public void loginOnclick(View view)
+    {
+        Intent intent=new Intent(this,loginPage.class);
+        startActivity(intent);
+    } 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/sam/wasteforbest/gmailSignUp.java
@@ -0,0 +1,13 @@
+package com.example.sam.wasteforbest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class gmailSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gmail_sign_up);
+    }
+}
